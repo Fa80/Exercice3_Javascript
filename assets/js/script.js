bd64527843c10cd2d55f6d3bb7963613dee28406
Fa <ngomfa@gmail.com>
2018-10-08 22:26:10 +0200

    exo3

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var FirstName = prompt("Entrez votre prenom :");
+
+alert("Bonjour," + Firstname);
+
+
+/*    Ceci est un test de condition
+
+var FirstName = prompt("Entrez votre prenom :");
+
+if (isNaN (FirstName)){
+    alert("Bonjour:" + FirstName);
+}
+else{
+  alert ("Erreur!");
+};
+*/
